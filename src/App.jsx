334bc8ac498931ac4895c69f461bb62cd2242950
sreerajesh008 <sreerajesh008@gmail.com>
2023-12-28 14:21:49 +0530

    login-ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import Home from "./components/home/home"
 import Explore from './components/explore/export'
 import Footer from './components/footer/footer'
 import Curosal from './components/curosal/curosal'
+import Login from './components/login/login'
+
+
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
      <div className='home'><Navbar></Navbar><Curosal></Curosal>
-     <Home></Home> <Explore></Explore> <Footer></Footer>
+     <Home></Home> <Explore></Explore> <Login></Login><Footer></Footer>
      </div>
   
   )
---
+++ src/components/login/login.jsx
@@ -0,0 +1,40 @@
+import"./login.css"
+
+
+
+
+
+const Login = () => {
+    return (
+      <div className="container">
+        <form id="loginForm" className="form">
+          <h2>Sign In</h2>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+
+          <button className="btn-1" type="submit">Sign In</button>
+          <p className="message"> Don't Have an Account?</p>
+        </form>
+        <div className="container-2">
+          <form id="signupForm" className="form">
+            <h2>Sign Up</h2>
+            <label htmlFor="newUsername">Username</label>
+            <input type="text" id="newUsername" name="newUsername" />
+
+            <label htmlFor="newPassword">Password</label>
+            <input type="password" id="newPassword" name="newPassword" />
+
+            <label htmlFor="newPassword"> Conform Password</label>
+            <input type="password" id="newPassword" name="newPassword" />
+
+            <button className="btn-1" type="submit">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    );
+}
+ 
+export default Login;
